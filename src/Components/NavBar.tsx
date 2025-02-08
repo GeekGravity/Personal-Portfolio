8ee44db2d51a7e097430f5e2ddc2c4a0c76be2c2
GeekGravity <mathewsjais20@gmail.com>
2025-02-07 18:58:53 -0800

    Styled the home page and added bg color to the website

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -5,7 +5,7 @@ function NavBar() {
     <>
       <div className="flex flex-row pt-12 ">
         <img src={logo} className="w-[120px] absolute left-10 py-1"></img>
-        <nav className="bg-orange-500 w-[550px] mx-auto rounded-[30px] shadow-md shadow-cyan-800">
+        <nav className="bg-orange-500 w-[550px] mx-auto rounded-[30px] shadow-md shadow-cyan-500">
           <ul className="flex flex-row justify-center py-3 text-white ">
             <li className="px-6 pb-1 text-lg">Home</li>
             <li className="px-6 pb-1 text-lg">About</li>
