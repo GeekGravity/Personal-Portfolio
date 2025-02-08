9944bce0f91eb6330cb8cfa66ad139f95a0e51fb
GeekGravity <mathewsjais20@gmail.com>
2025-02-07 18:52:51 -0800

    Added the profile picture

--- src/Sections/Home.tsx
+++ src/Sections/Home.tsx
@@ -3,12 +3,15 @@ import profilePic from "../Pictures/profilePic.jpg";
 function Home() {
   return (
     <>
-      <div className="grid grid-cols-3 gap-4">
-        <h1 className="text-[9rem] py-[400px] text-orange-400 px-15 leading-[160px] col-span-2">
+      <div className="grid grid-cols-3 gap-4 mx-auto">
+        <h1 className="col-span-2 my-[400px] mx-20 text-[9rem] text-orange-400 leading-[160px]">
           <span className="text-white">&lt; </span> WELCOME <br />
           &nbsp;TO MY PAGE! <span className="text-white"> / &gt;</span>
         </h1>
-        <img src={profilePic} className="w-[400px] py-[200px]"></img>
+        <img
+          src={profilePic}
+          className="w-[400px] my-[200px] mx-4 border-5 rounded-md border-white shadow-lg shadow-cyan-500"
+        ></img>
       </div>
     </>
   );
