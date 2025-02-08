41f514917566c5637e76670ad75258f0eceb8f79
GeekGravity <mathewsjais20@gmail.com>
2025-02-07 18:43:07 -0800

    Added the welcome message

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -3,14 +3,14 @@ import logo from "../Pictures/temp-logo.png";
 function NavBar() {
   return (
     <>
-      <div className="flex flex-row pt-10">
-        <img src={logo} className="w-[100px] absolute left-10 py-auto"></img>
-        <nav className="bg-orange-500 w-[500px] mx-auto rounded-[30px]">
+      <div className="flex flex-row pt-12 ">
+        <img src={logo} className="w-[120px] absolute left-10 py-1"></img>
+        <nav className="bg-orange-500 w-[550px] mx-auto rounded-[30px] shadow-md shadow-cyan-800">
           <ul className="flex flex-row justify-center py-3 text-white ">
-            <li className="px-6 pb-1">Home</li>
-            <li className="px-6 pb-1">About</li>
-            <li className="px-6 pb-1">Project</li>
-            <li className="px-6 pb-1">Experience</li>
+            <li className="px-6 pb-1 text-lg">Home</li>
+            <li className="px-6 pb-1 text-lg">About</li>
+            <li className="px-6 pb-1 text-lg">Project</li>
+            <li className="px-6 pb-1 text-lg">Experience</li>
           </ul>
         </nav>
       </div>
--- src/Sections/Home.tsx
+++ src/Sections/Home.tsx
@@ -1,9 +1,15 @@
-import NavBar from "../Components/NavBar";
+import profilePic from "../Pictures/profilePic.jpg";
 
 function Home() {
   return (
     <>
-      <NavBar></NavBar>
+      <div className="grid grid-cols-3 gap-4">
+        <h1 className="text-[9rem] py-[400px] text-orange-400 px-15 leading-[160px] col-span-2">
+          <span className="text-white">&lt; </span> WELCOME <br />
+          &nbsp;TO MY PAGE! <span className="text-white"> / &gt;</span>
+        </h1>
+        <img src={profilePic} className="w-[400px] py-[200px]"></img>
+      </div>
     </>
   );
 }
