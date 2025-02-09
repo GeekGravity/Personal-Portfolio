21edb0a49fc9e86ef75ad62f1fdf69fc5884dffa
GeekGravity <mathewsjais20@gmail.com>
2025-02-08 21:40:09 -0800

    Added the experience content placeholder

--- src/Sections/Experience.tsx
+++ src/Sections/Experience.tsx
@@ -1,46 +1,52 @@
 function Experience() {
+  const roles = [
+    {
+      Title: "Data Analyst Intern",
+      Date: "January '25 - Present",
+      Description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam ab ex dolor recusandae. Magni, corporis! Iusto earum officia repellat quae explicabo expedita dolor, iure quas consequuntur? Sed distinctio facilis eos! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem, ab expedita, aliquam nihil accusamus dolor in maiores quam facilis beatae illo voluptas sed excepturi repellendus? Nam accusantium ea inventore veniam?",
+    },
+    {
+      Title: "VP of Innovations",
+      Date: "Ocotober '25 - Present",
+      Description:
+        " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem, ab expedita, aliquam nihil accusamus dolor in maiores quam facilis beatae illo voluptas sed excepturi repellendus? Nam accusantium ea inventore veniam? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem, ab expedita, aliquam nihil accusamus dolor in maiores quam facilis beatae illo voluptas sed excepturi repellendus? Nam accusantium ea inventore veniam?",
+    },
+  ];
+
   return (
     <>
       <div className="my-[9%]">
         <h2 className="flex justify-center text-[5rem] text-white underline underline-offset-12 decoration-orange-400">
           Work Experience
         </h2>
-
-        <div className="relative mt-10">
-          {/* Timeline vertical line */}
-          <div className="absolute left-[50%] transform -translate-x-1/2 w-1 h-full bg-white"></div>
-
-          {/* Timeline Items */}
-          <div className="relative mb-12">
-            {/* Dot and Content Block */}
-            <div className="flex items-center">
-              <div className="w-[12px] h-[12px] bg-white rounded-full absolute left-[50%] transform -translate-x-1/2"></div>
-              <div className="ml-[5%] text-white">
-                <p className="text-orange-400 text-xl">October ‘24</p>
-                <p className="text-2xl font-bold">VP of Innovations</p>
-              </div>
-            </div>
+        <div className="flex justify-center py-20">
+          <div className="bg-white">
+            <button>
+              <a href="#role-0"> Hi sidhfoishdifh </a>
+            </button>
           </div>
-
-          <div className="relative">
-            <div className="flex items-center">
-              <div className="w-[12px] h-[12px] bg-white rounded-full absolute left-[50%] transform -translate-x-1/2"></div>
-              <div className="ml-[5%] text-white">
-                <p className="text-orange-400 text-xl">January ‘25</p>
-                <p className="text-2xl font-bold">Data Analysis Intern</p>
-              </div>
-            </div>
+          <div className="h-[400px] text-center px-12 overflow-y-scroll snap-y snap-mandatory no-scrollbar">
+            {roles.map((role, index) => {
+              return (
+                <section
+                  id={`role-${index}`}
+                  className="h-500px max-w-[900px] snap-center py-5 px-10"
+                >
+                  <h3 className="text-[2.5rem] text-white font-bold">
+                    {role.Title}
+                  </h3>
+                  <p className="text-[1.5rem] text-orange-400">{role.Date}</p>
+                  <p className=" py-10 text-2xl text-white">
+                    {role.Description}
+                  </p>
+                </section>
+              );
+            })}
           </div>
-        </div>
-
-        <div className="flex justify-center mt-10">
-          <button className="bg-orange-400 text-white text-[1.5em] py-1 px-6 rounded-2xl mb-10 w-[10%] text-lg shadow-md shadow-cyan-500">
-            Resume
-          </button>
         </div>
       </div>
     </>
   );
 }
-
 export default Experience;
