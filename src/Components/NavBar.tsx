80acf600ec6d123b03ef037683059ffdc1d66673
GeekGravity <mathewsjais20@gmail.com>
2025-02-12 16:30:43 -0800

    Added final touches

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -27,7 +27,11 @@ function NavBar() {
   return (
     <>
       <div id="nav" className="flex flex-row pt-12 ">
-        <img src={logo} className="w-[70px] absolute left-10 py-1"></img>
+        <img
+          src={logo}
+          alt="logo of MJ"
+          className="w-[70px] absolute left-10 py-1"
+        ></img>
         <nav className=" fixed top-12 left-1/2 -translate-x-1/2 w-[550px] mx-auto rounded-[30px] bg-orange-500 shadow-md shadow-cyan-500 z-40">
           <ul className="flex flex-row justify-center py-3 text-white ">
             <li className="px-6 pb-1 text-lg">
